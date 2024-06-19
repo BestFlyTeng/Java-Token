2893a7ace359456799f937548d51b3ca1398d6c9
Gallop <107476904+FlyTeng3049735932@users.noreply.github.com>
2024-06-19 16:44:55 +0800

    Add files via upload

---
+++ TokenUtils.java
@@ -0,0 +1,59 @@
+package com.best.common;
+
+import com.alibaba.fastjson.JSONObject;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class TokenUtils {
+  private static final String SIGN = StringUtils.TOKEN_SIGN;
+  private static final String ENCRYPTION = "HS256";
+
+  public static String getToken(Integer id, String email, String key) throws Exception {
+    JSONObject headJson = new JSONObject();
+    JSONObject payloadJson = new JSONObject();
+    headJson.put("typ", "JWT");
+    headJson.put("alg", ENCRYPTION);
+    payloadJson.put("id", id);
+    payloadJson.put("email", email);
+    String head = Base64.getEncoder().encodeToString(headJson.toString().getBytes());
+    String payload = Base64.getEncoder().encodeToString(payloadJson.toString().getBytes());
+    String message = head + payload;
+    return AESUtils.encrypt(key, head + "." + payload + "." + encodeHS256(message));
+  }
+
+  public static boolean parseToken(String token, String key) throws Exception {
+    try {
+      String decrypt = AESUtils.decrypt(key, token);
+      String[] parts = decrypt.split("\\.");
+      if (parts.length != 3) {
+        System.out.println("One False");
+        return false;
+      }
+      String headBase64 = new String(Base64.getUrlDecoder().decode(parts[0]), StandardCharsets.UTF_8);
+      String payloadBase64 = new String(Base64.getUrlDecoder().decode(parts[1]), StandardCharsets.UTF_8);
+      JSONObject headJson = JSONObject.parseObject(headBase64);
+      if (!ENCRYPTION.equals(headJson.get("alg")) || !"JWT".equals(headJson.get("typ"))) {
+        System.out.println("Two False");
+        return false;
+      }
+      if (!encodeHS256(Base64.getEncoder().encodeToString(headBase64.getBytes()) + Base64.getEncoder().encodeToString(payloadBase64.getBytes())).equals(parts[2])) {
+        System.out.println("There False");
+        return false;
+      }
+    } catch (Exception e) {
+      System.out.println("Four False");
+      return false;
+    }
+    return true;
+  }
+
+  private static String encodeHS256(String message) throws Exception {
+    Mac hmacSHA256 = Mac.getInstance("HmacSHA256");
+    SecretKeySpec secretKeySpec = new SecretKeySpec(SIGN.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+    hmacSHA256.init(secretKeySpec);
+    return Base64.getEncoder().encodeToString(hmacSHA256.doFinal(message.getBytes(StandardCharsets.UTF_8)));
+  }
+}
